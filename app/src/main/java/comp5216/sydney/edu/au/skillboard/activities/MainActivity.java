17f672f63fafd636a2d43a3386a90d83ab253615
Aravind Raj <aravindcraj@gmail.com>
2016-10-28 15:05:12 +0530

    Added authentication package and SigninActivity

--- app/src/main/java/comp5216/sydney/edu/au/skillboard/activities/MainActivity.java
+++ app/src/main/java/comp5216/sydney/edu/au/skillboard/activities/MainActivity.java
@@ -1,7 +1,9 @@
-package comp5216.sydney.edu.au.skillboard;
+package comp5216.sydney.edu.au.skillboard.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import comp5216.sydney.edu.au.skillboard.R;
 
 public class MainActivity extends AppCompatActivity {
 
